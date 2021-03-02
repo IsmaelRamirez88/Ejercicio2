8f9a3c7778e1f9b84fd1b59e292200daece56785
ISMAEL RAMIREZ <pbfront.eramirez@pueblobonito.com>
2021-03-01 19:44:35 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResiduo = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(97, 30);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 2;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(97, 71);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(107, 111);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Resultado: ";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(104, 156);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(16, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Residuo: ";
+            // 
+            // lblResiduo
+            // 
+            this.lblResiduo.AutoSize = true;
+            this.lblResiduo.Location = new System.Drawing.Point(104, 178);
+            this.lblResiduo.Name = "lblResiduo";
+            this.lblResiduo.Size = new System.Drawing.Size(16, 13);
+            this.lblResiduo.TabIndex = 1;
+            this.lblResiduo.Text = "...";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Mensaje: ";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(104, 201);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(16, 13);
+            this.lblMensaje.TabIndex = 1;
+            this.lblMensaje.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 242);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblResiduo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResiduo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblMensaje;
+    }
+}
+
---
+++ Ejercicio2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BuscaDivision()
+        {
+            double N1 = 0;
+            double N2 = 0;
+            double cos = 0;
+            double res = 0;
+            double Residuo = 0;
+            string mensaje = string.Empty;
+            string Es_O_No = string.Empty;
+            try
+            {
+                N1 = Convert.ToDouble(txtNumero1.Text);
+                N2 = Convert.ToDouble(txtNumero2.Text);
+
+                cos = (N1 / N2);
+                res = Math.Truncate(cos) * N2;
+                Residuo = N1 - res;
+                if (Residuo == 0)
+                {
+                    Es_O_No = "si";
+                }
+                else
+                {
+                    Es_O_No = "no";
+                }
+                mensaje = $"El número {N1} {Es_O_No} es divisible entre el número {N2}";
+
+                lblResultado.Text = cos.ToString();
+                lblResiduo.Text = Residuo.ToString();
+                lblMensaje.Text = mensaje;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            BuscaDivision();
+        }
+       
+
+    }
+}
+
+
